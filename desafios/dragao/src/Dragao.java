ffa04f902f3f97b78b1c244caea9bd9d7ff05fcf
ronissonpresidio <ronisson.dev@gmail.com>
2023-03-10 11:05:52 -0300

    Resolução do desafio dragão

---
+++ desafios/dragao/src/Dragao.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Dragao {
+    public static void main(String[] args) throws Exception {
+        int casos, poderDeLuta;
+
+        Scanner ler = new Scanner(System.in);
+
+        casos = ler.nextInt();
+
+        for (int i = 0; i < casos; i++) {
+            poderDeLuta = ler.nextInt();
+
+            if (poderDeLuta > 8000) {
+                System.out.println("Mais de 8000!");
+            } else {
+                System.out.println("Inseto!");
+            }
+
+        }
+    }
+}
